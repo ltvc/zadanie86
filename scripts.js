37058c4f7c98b66aa853499ebb7da1b02a130373
ltvc <lataviec666@gmail.com>
2018-05-18 12:30:33 +0200

    Add js and html file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var a=1, b=1;
+var value= (a*a) - (2*a*b)-(b*b);
+alert(value);
+console.log(value);
+if (value >0) {console.log('Wynik dodatni');}
+else if (value < 0) {console.log('Wynik ujemny');}
+else {console.log('Wynik rowniania-zero');}
