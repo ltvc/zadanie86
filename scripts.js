ca46f261b524f98d33b69ea99c4567ecf4d4ee5e
ltvc <38085145+ltvc@users.noreply.github.com>
2018-05-21 18:33:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-var a=1, b=1;
+var a=prompt ('Skrypt oblicza równanie; (a*a)-(2*a*b)-(b*b), podaj a:');
+var b=prompt ('podaj b:');
 var value= (a*a) - (2*a*b)-(b*b);
-alert(value);
+// alert(value);
 console.log(value);
-if (value >0) {console.log('Wynik dodatni');}
-else if (value < 0) {console.log('Wynik ujemny');}
-else {console.log('Wynik rowniania-zero');}
+if (value >0) {
+	alert ('Wynik dodatni='+value);
+	console.log('Wynik dodatni');}
+else if (value < 0) {
+	alert ('Wynik ujemny='+value);
+	console.log('Wynik ujemny');}
+else {
+	alert ('Wynik rowniania-zero='+value);
+	console.log('Wynik rowniania-zero');}
